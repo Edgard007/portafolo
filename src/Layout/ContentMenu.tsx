6a664c2e6bcc251a58f6a0f9c1fd3cf21a58ea45
Francisco Palacios <49413804+Edgard007@users.noreply.github.com>
2021-09-09 18:40:27 -0600

    💄 Add items to mobile menu

--- src/Layout/ContentMenu.tsx
+++ src/Layout/ContentMenu.tsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
+//* ==> Data <== *//
+import { data } from "./dataMenu";
+
 const ContentMenu = () => {
   return (
     <Wrapper>
-      <div className="containerLeft"></div>
+      <div className="containerLeft">
+        <div className="containerItems">
+          {data &&
+            data.map((el) => (
+              <span className="menu__items">{el?.namePath}</span>
+            ))}
+        </div>
+      </div>
       <div className="containerRigth"></div>
     </Wrapper>
   );
@@ -21,10 +31,51 @@ const Wrapper = styled.div`
 
   .containerLeft {
     width: 80%;
-    background-color: pink;
+    background-color: #ffff;
     height: 100vh;
     animation-duration: 0.5s;
-    animation-name: slideinOne;
+    animation-name: slideInRight;
+
+    display: flex;
+    align-items: center;
+  }
+
+  .containerItems {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    padding: 0px 25px;
+    width: 80%;
+    height: 80%;
+
+    animation-duration: 1.5s;
+    animation-name: appeared;
+    opacity: 1;
+  }
+
+  .menu__items {
+    position: relative;
+    color: #000000bf;
+    font-weight: bold;
+    font-size: 25px;
+    padding: 5px;
+    text-align: left;
+    margin-bottom: 20px;
+
+    &::before {
+      content: "";
+      position: absolute;
+      bottom: -0.5rem;
+      left: 0;
+      width: 0;
+      height: 4px;
+      background-color: ${(props) => props.theme?.primaryColor};
+      transition: all 0.3s;
+    }
+
+    &:hover::before {
+      width: 100%;
+    }
   }
 
   .containerRigth {
@@ -34,11 +85,11 @@ const Wrapper = styled.div`
     height: 100vh;
     background-color: #233df9;
     animation-duration: 1.5s;
-    animation-name: slidein;
+    animation-name: appeared;
     opacity: 1;
   }
 
-  @keyframes slideinOne {
+  @keyframes slideInRight {
     from {
       width: 0%;
     }
@@ -48,7 +99,7 @@ const Wrapper = styled.div`
     }
   }
 
-  @keyframes slidein {
+  @keyframes appeared {
     from {
       opacity: 0;
     }
